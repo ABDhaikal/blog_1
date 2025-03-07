201559aaac1823b0c9befe1fef9415ea00c6fa8b
ABDhaikal <kakikuatku.gmail.com>
2025-03-07 13:50:55 +0700

    feat:search

--- src/app/blog/hooks/useGetBlogs.tsx
+++ src/app/blog/hooks/useGetBlogs.tsx
@@ -1,27 +1,46 @@
 "use client ";
 import { BASE_DB_URL } from "@/app/api/blogApi/api";
 import { Blog } from "@/app/types/blog";
-import { tr } from "date-fns/locale";
-import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const useGetBlogs = async () => {
-   const [data, setData] = useState<Blog[]>([]);
-   const [isLoading, setisLoading] = useState<boolean>(true);
+// const useGetBlogs = async () => {
+//    const [data, setData] = useState<Blog[]>([]);
+//    const [isLoading, setisLoading] = useState<boolean>(true);
 
-   const getData = async () => {
-      try {
-         const response = await fetch(`${BASE_DB_URL}`);
-         const data: Blog[] = await response.json();
-         setData(data);
-      } catch (error) {
-         
-      } finally {
-         setisLoading(false);
-      }
-      return data;
-   };
+//    const getData = async () => {
+//       try {
+//          const response = await fetch(`${BASE_DB_URL}`);
+//          const data: Blog[] = await response.json();
+//          setData(data);
+//       } catch (error) {
 
-   return { data, isLoading, getData };
+//       } finally {
+//          setisLoading(false);
+//       }
+//       return data;
+//    };
+
+//    return { data, isLoading, getData };
+// };
+
+// export default useGetBlogs;
+
+interface getBlogQuery {
+   title: string;
+}
+
+const useGetBlogs = (queries: getBlogQuery) => {
+   return useQuery({
+      queryKey: ["blogs", queries], // consider adding a unique identifier or a dependency array to ensure the query is re-run when necessary
+      queryFn: async () => {
+         const { title } = queries;
+         const { data } = await axios.get<Blog[]>(
+            `https://givingairport-us.backendless.app/api/data/blogs?where=%60title%60%20LIKE%20'%25${title}%25'`
+         ); // consider adding a specific endpoint for blogs
+         return data;
+      },
+   });
 };
 
 export default useGetBlogs;
--- src/app/types/blog.ts
+++ src/app/types/blog.ts
@@ -2,11 +2,10 @@ export interface Blog {
    objectId: string;
    title: string;
    description: string;
-   image: string;
    slug: string;
    category: string;
    thumbnail: string;
    created: number;
    content: string;
-   views: number;
+   views: number|null;
 }
